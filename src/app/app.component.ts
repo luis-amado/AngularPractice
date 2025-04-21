109fb3c727ee21b7a73dcad72c743612f25102b3
Luis <lamadoalvarez@gmail.com>
2025-04-21 11:27:39 -0600

    Primera clase de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BotonComponent } from './boton/boton.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BotonComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Angular';
---
+++ src/app/boton/boton.component.ts
@@ -0,0 +1,11 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-boton',
+  imports: [],
+  templateUrl: './boton.component.html',
+  styleUrl: './boton.component.css',
+})
+export class BotonComponent {
+  text: string = 'dale click';
+}
